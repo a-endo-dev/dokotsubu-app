e19bc5919c83bf78809cb271ff64edb1474335a6
endo0724 <sct202511miyagi.akina.endo@gmail.com>
2026-04-10 14:53:34 +0900

    初回コミット（Java版つぶやきアプリ）

---
+++ dokoTubu/src/main/java/servlet/Main.java
@@ -0,0 +1,89 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import model.Mutter;
+import model.PostMutterLogic;
+import model.User;
+
+@WebServlet("/Main")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		ServletContext application = this.getServletContext();
+		List<Mutter> mutterList =
+				(List<Mutter>) application.getAttribute("mutterList");
+
+		HttpSession session = request.getSession();
+		User loginUser = (User) session.getAttribute("loginUser");
+
+		if (loginUser == null) {
+			response.sendRedirect("index.jsp");
+		} else {
+			request.setAttribute("mutterList", mutterList);
+
+			RequestDispatcher dispatcher =
+					request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+
+		if (text != null && text.length() != 0) {
+
+			ServletContext application = this.getServletContext();
+			List<Mutter> mutterList =
+					(List<Mutter>) application.getAttribute("mutterList");
+
+			if (mutterList == null) {
+				mutterList = new ArrayList<Mutter>();
+			}
+
+			HttpSession session = request.getSession();
+			User loginUser = (User) session.getAttribute("loginUser");
+
+			Mutter mutter = new Mutter(loginUser.getName(), text);
+
+			PostMutterLogic postMutterLogic = new PostMutterLogic();
+			postMutterLogic.execute(mutter, mutterList);
+
+			application.setAttribute("mutterList", mutterList);
+
+			response.sendRedirect("Main");
+
+		} else {
+			ServletContext application = this.getServletContext();
+			List<Mutter> mutterList =
+					(List<Mutter>) application.getAttribute("mutterList");
+
+			HttpSession session = request.getSession();
+			User loginUser = (User) session.getAttribute("loginUser");
+
+			request.setAttribute("mutterList", mutterList);
+			request.setAttribute("errorMsg", "つぶやきが入力されていません");
+
+			RequestDispatcher dispatcher =
+					request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+}
